21cd13d4beb1cd531f70ad4035f536121f4012ce
Alan Rominger <arominge@redhat.com>
2025-06-25 15:57:20 -0400

    use content_object as intended

--- example_project/src/example_project/views.py
+++ example_project/src/example_project/views.py
@@ -1,15 +1,17 @@
-import json
-from urllib.request import urlopen
 from django.http import JsonResponse
-from example_project.testapp.models import Book
+
+from example_project.testapp.models import Reference
 
 
 def unified_books(request):
-    local = list(Book.objects.values("id", "title"))
-    with urlopen("http://localhost:8001/books/") as resp:
-        remote = json.load(resp)
-    combined = [{"id": b["id"], "title": b["title"], "source": "local"} for b in local]
-    combined += [
-        {"id": b["id"], "title": b["title"], "source": "remote"} for b in remote
-    ]
-    return JsonResponse(combined, safe=False)
+    """Return a list of local and remote books using ``Reference`` objects."""
+
+    books = []
+    for ref in Reference.objects.all().select_related("content_type"):
+        obj = ref.content_object
+        if hasattr(obj, "fetch"):
+            data = obj.fetch()
+            books.append({"id": obj.object_id, "title": data["title"], "source": "remote"})
+        else:
+            books.append({"id": obj.pk, "title": obj.title, "source": "local"})
+    return JsonResponse(books, safe=False)
