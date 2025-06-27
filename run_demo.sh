77a470264201307dafe3687c9037bc183b34b337
AlanCoding <arominge@redhat.com>
2025-06-27 16:19:31 -0400

    Get rid of dir change in demo

--- run_demo.sh
+++ run_demo.sh
@@ -5,20 +5,18 @@ pip install -e . >/dev/null
 pip install -e example_project >/dev/null
 pip install -e remote_project >/dev/null
 
-(cd remote_project && \
-    DATABASE_NAME=db_remote.sqlite3 ./manage.py migrate --noinput && \
-    DATABASE_NAME=db_remote.sqlite3 ./manage.py shell -c "from remote_project.remoteapp.models import Book; Book.objects.create(title='Remote Book 1');" && \
-    DATABASE_NAME=db_remote.sqlite3 ./manage.py runserver 8001 &)
+(DATABASE_NAME=db_remote.sqlite3 ./remote_project/manage.py migrate --noinput && \
+    DATABASE_NAME=db_remote.sqlite3 ./remote_project/manage.py shell -c "from remote_project.remoteapp.models import Book; Book.objects.create(title='Remote Book 1');" && \
+    DATABASE_NAME=db_remote.sqlite3 ./remote_project/manage.py runserver 8001 &)
 REMOTE_PID=$!
 
 sleep 3
 
-(cd example_project && \
-    DATABASE_NAME=db_example.sqlite3 ./manage.py migrate --noinput && \
-    DATABASE_NAME=db_example.sqlite3 ./manage.py shell -c "from example_project.testapp.models import Book; Book.objects.create(title='Local Book 1')" && \
-    DATABASE_NAME=db_example.sqlite3 ./manage.py sync_local_books && \
-    DATABASE_NAME=db_example.sqlite3 ./manage.py sync_remote_books && \
-    DATABASE_NAME=db_example.sqlite3 ./manage.py runserver 8000 &)
+(DATABASE_NAME=db_example.sqlite3 ./example_project/manage.py migrate --noinput && \
+    DATABASE_NAME=db_example.sqlite3 ./example_project/manage.py shell -c "from example_project.testapp.models import Book; Book.objects.create(title='Local Book 1')" && \
+    DATABASE_NAME=db_example.sqlite3 ./example_project/manage.py sync_local_books && \
+    DATABASE_NAME=db_example.sqlite3 ./example_project/manage.py sync_remote_books && \
+    DATABASE_NAME=db_example.sqlite3 ./example_project/manage.py runserver 8000 &)
 LOCAL_PID=$!
 
 echo "Example project available at http://localhost:8000/books/"
